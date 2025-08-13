815c7df89c36a94e6dd8caa9f697c2567582b7cd
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-13 01:50:43 +0100

    Api error

--- lib/provider.ts
+++ lib/provider.ts
@@ -1,5 +1,11 @@
 import { JsonRpcProvider } from 'ethers'
 
-const provider = new JsonRpcProvider(process.env.BASE_RPC_URL)
+const rpcUrl = process.env.NEXT_PUBLIC_BASE_RPC_URL
+
+if (!rpcUrl) {
+  throw new Error('Missing NEXT_PUBLIC_BASE_RPC_URL in environment variables')
+}
+
+const provider = new JsonRpcProvider(rpcUrl)
 
 export default provider
